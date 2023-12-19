7764c330a32af66876a569787db0ba2e6b018780
Kokoteros <marcosjose.piedra@gmail.com>
2023-12-19 00:30:57 -0300

    commit

---
+++ src/main/java/com/info/streamingmusic/StreamingmusicApplication.java
@@ -0,0 +1,13 @@
+package com.info.streamingmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/info/streamingmusic/StreamingmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.streamingmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
